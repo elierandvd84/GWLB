cf72dd4fb7bfdc63b7de1033052e505656fe7894
Maor Yahalomi <maory@radware.com>
2020-12-02 13:57:22 +0200

    commit

--- get_baselines.py
+++ get_baselines.py
@@ -5,34 +5,38 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 #Login to vision
 
-login = "https://44.233.145.252/mgmt/system/user/login"
+def GetBaseline():
+  
+    vision_url = "https://44.233.145.252/mgmt/system/user/login"
 
-switchuser='radware'
-switchpassword='radware'
-myheaders={'content-type':'application/json','Accept':'*/*','Accept-Encoding':'gzip, deflate, br','Connection':'keep-alive'}
+    switchuser='radware'
+    switchpassword='radware'
+    myheaders={'content-type':'application/json','Accept':'*/*','Accept-Encoding':'gzip, deflate, br','Connection':'keep-alive'}
+    payload={
+        "username": "radware", 
+        "password": "radware"
+    }
+    res = requests.post(vision_url,data=json.dumps(payload),verify=False,headers=myheaders,auth=(switchuser,switchpassword))
+    reposne_back = res.json()
+    cookie = reposne_back["jsessionid"]
+    
 
-payload={
-    "username": "radware", 
-    "password": "radware"
- }
+    cookie_srever_format = "JSESSIONID="+cookie+";"+"JSESSIONID="+cookie
 
+    print("Get DP 1 BDoS baselinse...")
+    baseline='https://44.233.145.252/mgmt/device/byip/10.10.10.56/config/getnetworktemplate?PolicyName=any&ExportConfiguration=off&ExportBaselineDNS=off&ExportBaselineBDoS=on&ExportBaselineHttpsFlood=off&ExportSigUsrProf=off&ExportTrafficFiltersProf=off&ExportAntiScanWhitelists=off&saveToDb=false&fileName=DP_MR20_POC_Demo_2020.11.26_17.03.55'
+    myheaders2={'content-type':'application/json','Accept':'*/*','Accept-Encoding':'gzip, deflate, br','Connection':'keep-alive','Cookie':cookie_srever_format}
+    params={"token": cookie}
 
-res = requests.post(login,data=json.dumps(payload),verify=False,headers=myheaders,auth=(switchuser,switchpassword))
-reposne_back = res.json()
-cookie = reposne_back["jsessionid"]
+    response = requests.get(baseline,data=json.dumps(payload),verify=False,params=params,headers=myheaders2)
+    response_string = response.text
+    print(response_string)
+    return response_string
 
+DP_Baselinse = GetBaseline()
 
-cookie_srever_format = "JSESSIONID="+cookie+";"+"JSESSIONID="+cookie
-
-print("Get DP 1 baselinse")
-baseline='https://44.233.145.252/mgmt/device/byip/10.10.10.56/config/getnetworktemplate?PolicyName=any&ExportConfiguration=off&ExportBaselineDNS=off&ExportBaselineBDoS=on&ExportBaselineHttpsFlood=off&ExportSigUsrProf=off&ExportTrafficFiltersProf=off&ExportAntiScanWhitelists=off&saveToDb=false&fileName=DP_MR20_POC_Demo_2020.11.26_17.03.55'
-myheaders2={'content-type':'application/json','Accept':'*/*','Accept-Encoding':'gzip, deflate, br','Connection':'keep-alive','Cookie':cookie_srever_format}
-params={"token": cookie}
-
-response = requests.get(baseline,data=json.dumps(payload),verify=False,params=params,headers=myheaders2)
-print(response.text)
-udp_baselin = response.text.splitlines()
+udp_baselin = DP_Baselinse.splitlines()
 udp_baselin_rate = udp_baselin[3].split()
 print("\n"*3)
 # print(udp_baselin_rate[1])
-print("maor")
+
